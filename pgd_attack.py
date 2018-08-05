774c052cc91df1255c2ed21f8faf607b8aefbec8
Michael Sun <micsun@mit.edu>
2018-08-04 22:32:31 -0400

    initial l2 commit with command line args

--- pgd_attack.py
+++ pgd_attack.py
@@ -10,8 +10,13 @@ from __future__ import print_function
 import tensorflow as tf
 import numpy as np
 
+import argparse # adding parser for config file
+parser = argparse.ArgumentParser(description='Train an adversarial network according to the specified config file')
+parser.add_argument('-c', '--config', type=str, default='config.json', help='path to the config file to train the adversarial network')
+args = parser.parse_args()
 
-class LinfPGDAttack:
+
+class PGDAttack:
   def __init__(self, model, epsilon, k, a, random_start, loss_func):
     """Attack parameter initialization. The attack performs k steps of
        size a, while always staying within epsilon from the initial
@@ -51,12 +56,34 @@ class LinfPGDAttack:
       grad = sess.run(self.grad, feed_dict={self.model.x_input: x,
                                             self.model.y_input: y})
 
-      x += self.a * np.sign(grad)
-
-      x = np.clip(x, x_nat - self.epsilon, x_nat + self.epsilon) 
-      x = np.clip(x, 0, 1) # ensure valid pixel range
+      if self.norm == 'inf':
+          x += self.a * np.sign(grad) # update
+          x = np.clip(x, x_nat - self.epsilon, x_nat + self.epsilon) # project
+      else: # two
+          # update
+          grad_norm = self._l2_norm(grad)
+          grad_norm = grad_norm.clip(1e-8, np.inf) # protect against zero grad
+          x += self.a * grad / grad_norm
+          # project
+          dx = x - x_nat
+          dx_norm = self._l2_norm(dx)
+          dx_final_norm = dx_norm.clip(0, self.epsilon)
+          x = x_nat + dx_final_norm * dx / dx_norm
 
     return x
+
+  @staticmethod
+  def _l2_norm(batch):
+      return np.sqrt((batch ** 2).sum(axis=1, keepdims=True))
+
+
+  @staticmethod
+  def _l2_ball_sample(radius, shape):
+      direction = np.random.normal(0.0, 1.0, shape)
+      direction = direction / __class__._l2_norm(direction)
+      dimension = np.prod(shape[1:]) # product of non-batch dimensions
+      magnitude = radius * np.random.uniform(0, 1, (shape[0],1)) ** (1 / dimension)
+      return magnitude * direction
 
 
 if __name__ == '__main__':
@@ -68,7 +95,7 @@ if __name__ == '__main__':
 
   from model import Model
 
-  with open('config.json') as config_file:
+  with open(args.config) as config_file:
     config = json.load(config_file)
 
   model_file = tf.train.latest_checkpoint(config['model_dir'])
@@ -77,12 +104,12 @@ if __name__ == '__main__':
     sys.exit()
 
   model = Model()
-  attack = LinfPGDAttack(model,
-                         config['epsilon'],
-                         config['k'],
-                         config['a'],
-                         config['random_start'],
-                         config['loss_func'])
+  attack = PGDAttack(model,
+                     config['epsilon'],
+                     config['k'],
+                     config['a'],
+                     config['random_start'],
+                     config['loss_func'])
   saver = tf.train.Saver()
 
   mnist = input_data.read_data_sets('MNIST_data', one_hot=False)
