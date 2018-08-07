7577d62c26be3063163cc20c0c3d05a735037e1a
Michael Sun <micsun@mit.edu>
2018-08-07 00:13:05 -0400

    adding attack configs and modifying pgd attack to use multiple random restarts

--- pgd_attack.py
+++ pgd_attack.py
@@ -29,7 +29,7 @@ class PGDAttack:
     self.rand = random_start
 
     if loss_func == 'xent':
-      loss = model.xent
+      self.loss = model.xent
     elif loss_func == 'cw':
       label_mask = tf.one_hot(model.y_input,
                               10,
@@ -38,40 +38,52 @@ class PGDAttack:
                               dtype=tf.float32)
       correct_logit = tf.reduce_sum(label_mask * model.pre_softmax, axis=1)
       wrong_logit = tf.reduce_max((1-label_mask) * model.pre_softmax, axis=1)
-      loss = -tf.nn.relu(correct_logit - wrong_logit + 50)
+      self.loss = -tf.nn.relu(correct_logit - wrong_logit + 50)
     else:
       print('Unknown loss function. Defaulting to cross-entropy')
-      loss = model.xent
+      self.loss = model.xent
 
-    self.grad = tf.gradients(loss, model.x_input)[0]
+    self.grad = tf.gradients(self.loss, model.x_input)[0]
 
-  def perturb(self, x_nat, y, sess):
+  def perturb(self, x_nat, y, sess, iters=1):
     """Given a set of examples (x_nat, y), returns a set of adversarial
        examples within epsilon of x_nat in l_infinity norm."""
-    if self.rand:
-      x = x_nat + np.random.uniform(-self.epsilon, self.epsilon, x_nat.shape)
-    else:
-      x = np.copy(x_nat)
 
-    for i in range(self.k):
-      grad = sess.run(self.grad, feed_dict={self.model.x_input: x,
-                                            self.model.y_input: y})
+    best_x = x_nat
+    best_loss = sess.run(self.loss, feed_dict={self.model.x_input: x_nat,
+                                              self.model.y_input: y})
+    for it in range(iters):
+      if self.rand:
+        x = x_nat + np.random.uniform(-self.epsilon, self.epsilon, x_nat.shape)
+      else:
+        x = np.copy(x_nat)
 
-      if self.norm == 'inf':
-          x += self.a * np.sign(grad) # update
-          x = np.clip(x, x_nat - self.epsilon, x_nat + self.epsilon) # project
-      else: # two
-          # update
-          grad_norm = self._l2_norm(grad)
-          grad_norm = grad_norm.clip(1e-8, np.inf) # protect against zero grad
-          x += self.a * grad / grad_norm
-          # project
-          dx = x - x_nat
-          dx_norm = self._l2_norm(dx)
-          dx_final_norm = dx_norm.clip(0, self.epsilon)
-          x = x_nat + dx_final_norm * dx / dx_norm
-      x = np.clip(x, 0, 1)
-    return x
+      for i in range(self.k):
+        grad = sess.run(self.grad, feed_dict={self.model.x_input: x,
+                                              self.model.y_input: y})
+
+        if self.norm == 'inf':
+            x += self.a * np.sign(grad) # update
+            x = np.clip(x, x_nat - self.epsilon, x_nat + self.epsilon) # project
+        else: # two
+            # update
+            grad_norm = self._l2_norm(grad)
+            grad_norm = grad_norm.clip(1e-8, np.inf) # protect against zero grad
+            x += self.a * grad / grad_norm
+            # project
+            dx = x - x_nat
+            dx_norm = self._l2_norm(dx)
+            dx_final_norm = dx_norm.clip(0, self.epsilon)
+            x = x_nat + dx_final_norm * dx / dx_norm
+        x = np.clip(x, 0, 1)
+
+      new_loss = sess.run(self.loss, feed_dict={self.model.x_input: x,
+                                                self.model.y_input: y})
+      if new_loss > best_loss:
+        best_loss = new_loss
+        best_x = x
+
+    return best_x
 
   @staticmethod
   def _l2_norm(batch):
